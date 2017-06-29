59888119af06b97a47a5701f07fc7a2a92e3fc25
Atharva Khandait <akhandait45@gmail.com>
2017-06-30 03:27:55 +0530

    Style modifications

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -4,42 +4,37 @@ import java.util.Iterator;
 public class RandomizedQueue<Item> implements Iterable<Item> {
 	private Item[] A;
 	private int n = 0;
-	public RandomizedQueue()
-	{
+	
+	public RandomizedQueue() {
 		A = (Item[]) new Object[1];
 	}
 	
-	public boolean isEmpty()
-	{
+	public boolean isEmpty() {
 		return n==0;
 	}
 
-	public int size()
-	{
+	public int size() {
 		return n;
 	}
 	
-	public void enqueue(Item item)
-	{
-		if(item == null) throw new NullPointerException();
-		if(n == A.length) resize(2*A.length);
+	public void enqueue(Item item) {
+		if (item == null) throw new NullPointerException();
+		if (n == A.length) resize(2*A.length);
 		A[n] = item;
 		n++;
 	}
 	
-	private void resize(int capacity)
-	{
+	private void resize(int capacity) {
 		Item[] copy = (Item[]) new Object[capacity];
-		for(int i=0; i < n; i++)
-		{
+		for (int i = 0; i < n; i++) {
 			copy[i] = A[i];
 		}
 		A = copy;
 	}
 	
-	public Item dequeue()
-	{
-		if(n == 0) throw new java.util.NoSuchElementException();
+	public Item dequeue() {
+		if (n == 0) throw new java.util.NoSuchElementException();
+		
 		int r = StdRandom.uniform(n);
 		swap(r, n-1);
 		Item ret = A[n-1];
@@ -49,40 +44,35 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 		return ret;
 	}
 	
-	private void swap(int a, int b)
-	{
+	private void swap(int a, int b) {
 		Item temp = A[a];
 		A[a] = A[b];
 		A[b] = temp;
 	}
 	
-	public Item sample()
-	{
-		if(n == 0) throw new java.util.NoSuchElementException();
+	public Item sample() {
+		if (n == 0) throw new java.util.NoSuchElementException();
+		
 		int r = StdRandom.uniform(n);
 		return A[r];
 	}
 	
-	public Iterator<Item> iterator()
-	{
+	public Iterator<Item> iterator() {
 		return new RAIterator();
 	}
 	
-	private class RAIterator implements Iterator<Item>
-	{
+	private class RAIterator implements Iterator<Item> {
 		private int p = n;
 		private Item[] B = A;
-		public boolean hasNext()
-		{
+		public boolean hasNext() {
 			return p > 0;
 		}
-		public void remove()
-		{
+		public void remove() {
 			throw new java.lang.UnsupportedOperationException();
 		}
-		public Item next()
-		{
-			if(!hasNext()) throw new java.util.NoSuchElementException();
+		public Item next() {
+			if (!hasNext()) throw new java.util.NoSuchElementException();
+			
 			int r = StdRandom.uniform(p);
 			swap(r, p-1); Item ret = B[p-1]; p--;
 			return ret;
