4b6e1a8b9e28fb3d161f46af69dc9ab8b4d6d4a5
Atharva Khandait <akhandait45@gmail.com>
2017-06-30 03:24:17 +0530

    Style modifications

--- Permutation.java
+++ Permutation.java
@@ -2,17 +2,14 @@ import java.util.Iterator;
 import edu.princeton.cs.algs4.*;
 
 public class Permutation {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		RandomizedQueue R = new RandomizedQueue();
-		for (String s : args)
-		{
+		for (String s : args) {
 			try{Integer.parseInt(s);} catch(NumberFormatException e) {R.enqueue(s);}
 		}
 		Iterator itr = R.iterator();
-		for(int i=0; i<n; i++)
-		{
+		for (int i = 0; i < n; i++) {
 			StdOut.println(itr.next());
 		}
 	}
